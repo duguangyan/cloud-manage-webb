23154482172118f79b31e2c525b9a5bf391c5f24
李世其 <756850692@qq.com>
2019-08-16 17:37:36 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -36,18 +36,18 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
-    },
-    proxy: {
-      // change xxx-api/login => mock/login
-      // detail: https://cli.vuejs.org/config/#devserver-proxy
-      [process.env.VUE_APP_BASE_API]: {
-        target: 'http://192.168.0.202:8000',
-        changeOrigin: true,
-        pathRewrite: {
-          ['^' + process.env.VUE_APP_BASE_API]: ''
-        }
-      }
     }
+    // proxy: {
+    //   // change xxx-api/login => mock/login
+    //   // detail: https://cli.vuejs.org/config/#devserver-proxy
+    //   [process.env.VUE_APP_BASE_API]: {
+    //     target: 'http://192.168.0.202:8000',
+    //     changeOrigin: true,
+    //     pathRewrite: {
+    //       ['^' + process.env.VUE_APP_BASE_API]: ''
+    //     }
+    //   }
+    // }
     // after: require('./mock/mock-server.js')
   },
   configureWebpack: {
